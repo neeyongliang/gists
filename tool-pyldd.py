107416a8fe8bf60bd0cc296a20b2e93ff466bad2
yongliang <neeyongliang@gmail.com>
2022-06-26 12:59:27 +0800

    Update pyldd: use argparse parse commandline argument

--- tool-pyldd.py
+++ tool-pyldd.py
@@ -2,43 +2,59 @@
 # -*- coding: utf-8 -*-
 
 """
-Time    : 2022.06.14
+Time    : 2022.06.26
 Author  : neeyongliang
-File    : tool-pyldd.py
-Descript: write auto check and list dynamic libraries for ldd
+Descript: A enhanced version for linux ldd by python
 """
 import os
 import sys
 import subprocess
-import shutil
+# import shutil
+import platform
+import argparse
 
 
 class PyLdd(object):
-    def __init__(self) -> None:
+    def __init__(self, sys_ld=None, do_copy=False) -> None:
         self.cmd_path = ""
         self.raw_libs = []
         self.all_libs = []
         self.paths = []
+        self.arch = platform.machine()
+        self.failed_libs = []
+        self.do_copy = do_copy
+        if not sys_ld:
+            self.sys_ld = "/lib/{}-linux-gnu/ld-2.31.so".format(self.arch)
+        else:
+            self.sys_ld = sys_ld
 
     def pre_check(self, cmd_path):
         self.cmd_path = cmd_path
         if not os.path.exists(self.cmd_path):
-            self.print_usage()
+            print("Error: cannot find at %s", cmd_path)
+            return False
+        if not os.path.exists(self.sys_ld):
+            print("Error: cannot find system ld file at: ", self.sys_ld)
             return False
         return True
 
     def get_raw_libs(self):
         try:
-            cmd_str = "ldd " + self.cmd_path
+            cmd_str = "{} {} {}".format("LD_TRACE_LOADED_OBJECTS=1",
+                                        self.sys_ld, self.cmd_path)
             p = subprocess.check_output(cmd_str, shell=True)
             raw_output = p.decode().replace('\t', '\n')
             outputs = raw_output.split('\n')
             self.raw_libs = []
+            self.failed_libs = []
             for output in outputs:
                 if not output:
                     continue
                 items = output.split(' ')
                 if len(items) != 4:
+                    continue
+                if items[2] == "not":
+                    self.failed_libs.append(items[0])
                     continue
                 self.raw_libs.append(items[2])
             # print(self.raw_libs)
@@ -60,7 +76,6 @@ class PyLdd(object):
                 tmp = os.path.join(raw_lib_dir, os.readlink(raw_lib))
                 self.all_libs.append(tmp)
                 raw_lib = tmp
-
         # print(self.all_libs)
 
     def copy_libs(self):
@@ -70,30 +85,55 @@ class PyLdd(object):
                 cmd = "cp -a {} {}".format(lib,
                                            os.path.join(os.getcwd(),
                                                         lib_dir[1:]))
-                print(cmd)
+                if self.do_copy:
+                    subprocess.call(cmd, shell=True)
+                else:
+                    print(cmd)
             else:
-                print("warn: cannot find: ", lib)
+                print("Error: cannot find ", lib)
 
-    def single_parse(self, cmd_path):
-        self.pre_check(cmd_path)
+    def collect_failed(self):
+        if not self.failed_libs:
+            return
+        print("Failed list:")
+        for lib in self.failed_libs:
+            print(lib)
+        print("Find above library path, and add it to $LD_LIBRARY_PATH env")
+
+    def single_parse(self, file_path):
+        if not self.pre_check(file_path):
+            return
         self.get_raw_libs()
         self.get_all_libs()
         self.copy_libs()
+        self.collect_failed()
 
-    def run(self, cmds):
-        for cmd in cmds:
-            self.single_parse(cmd)
+    def run(self, files=None):
+        for file in files:
+            self.single_parse(file)
 
     def print_usage(self):
-        print("tool-get-link-target.py BINARY_PATH")
+        print(sys.argv[0], " BINARY_PATH")
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Ldd enhanced tool by python')
+    parser.add_argument('-l', '--ld', type=str,
+                        help="Cannot found ld-x.xx.so path")
+    parser.add_argument('-n', '--nocopy', action='store_false',
+                        help="Do not really copy, False default")
+    parser.add_argument('-f', '--files', type=str, nargs='*', required=True,
+                        help="ELF binary file path")
+    parser.add_argument('-v', '--version', action='version',
+                        version="1.0, create by neeyongliang for Linux ❤️")
+    args = parser.parse_args()
+    return args
 
 
 if __name__ == '__main__':
     try:
-        pyldd = PyLdd()
-        if len(sys.argv) > 1:
-            pyldd.run(sys.argv[1:])
-        else:
-            pyldd.print_usage()
+        inputs = parse_args()
+        pyldd = PyLdd(inputs.ld, inputs.nocopy)
+        pyldd.run(files=inputs.files)
     except Exception as e:
-        print("Ooooops: something wrong, ", e)
+        print("Oooooooooopt, because ", e)
