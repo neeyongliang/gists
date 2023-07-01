29a7e4843b2649039c5a7ae537bff38690326238
yongliang <neeyongliang@gmail.com>
2023-07-01 21:55:30 +0800

    Add generate mdbook pdf file script
    Update create ramdisk script
    Update README.md

--- README.md
+++ README.md
@@ -1,29 +1,6 @@
-# My Code Snippet
-Write fool code, be a intelligent person.
+# 代码片段
 
-If one thing can do use script automatically, do not again by manual.
-
-## Tool
-- tool-code-count.py: code count for debian package.
-- tool-git-filter-branch.sh: rewrite all commit information.
-- tool-arp-listener.go: network arp listener by golang.
-- tool-pyldd.py: enhanced version ldd by python.
-
-## Script
-- script-install-elk-stack.sh: a script for install Elastic, Logstash and Kibana.
-- script-create-ramdisk.sh：a script for create ramdisk, provide two method.
-- script-create-ramfs.sh：a script for create ramfs.
-- script-project-release.sh：a script for project release package.
-- script-qemu-nographic.sh：a script for qmeu to test rootfs quickly.
-
-## Patch
-- patch-qt-5.6.3-webkit-backport.patch：QtWebkit 5.6.3 backport patch.
-
-# 我的代码片段
-笨拙编码，智慧做人。
-
-如果一件事情可以使用代码自动化的完成，就不要手动再做一次。
-
+笨拙编码，智慧做人，如果一件事情可以使用代码自动化的完成，就不要手动再做一次。
 
 ## 工具类
 - tool-code-count.py: 一种 debian 包的代码统计工具。
@@ -37,6 +14,27 @@ If one thing can do use script automatically, do not again by manual.
 - script-create-ramfs.sh：创建 ramfs 的脚本。
 - script-project-release.sh：为项目发布打包的脚本。
 - script-qemu-nographic.sh：使用 qmeu 快速测试 rootfs 的脚本。
+- script-generate-mdbook-pdf.sh：创建 mdbook 的 PDF 版本。
 
 ## 补丁类
 - patch-qt-5.6.3-webkit-backport.patch：QtWebKit 5.6.3 组件的兼容性补丁。
+
+# Code Snippet
+Write fool code, be a intelligent person. If one thing can do use script automatically, do not again by manual.
+
+## Tool
+- tool-code-count.py: code count for debian package.
+- tool-git-filter-branch.sh: rewrite all commit information.
+- tool-arp-listener.go: network arp listener by golang.
+- tool-pyldd.py: enhanced version ldd by python.
+
+## Script
+- script-install-elk-stack.sh: a script for install Elastic, Logstash and Kibana.
+- script-create-ramdisk.sh：a script for create ramdisk, provide two method.
+- script-create-ramfs.sh：a script for create ramfs.
+- script-project-release.sh：a script for project release package.
+- script-qemu-nographic.sh：a script for qmeu to test rootfs quickly.
+- script-generate-mdbook-pdf.sh：a script for generate pdf file.
+
+## Patch
+- patch-qt-5.6.3-webkit-backport.patch：QtWebkit 5.6.3 backport patch.
--- script-create-ramdisk.sh
+++ script-create-ramdisk.sh
@@ -2,7 +2,7 @@
 
 # Description: this is a script for create ramdisk
 # Author: neeyongliang
-# Date: 2022.06.18 First release
+# Changelog: 2022.06.18 First release
 
 set -e
 
@@ -11,55 +11,54 @@ gzFileName="rootfs.ramdisk.gz"
 ubootFileName="rootfs.ramdisk.gz.uboot"
 imgFileName="rootfs.img"
 
-create_ramdisk() {
-	if [ -f $fileName ]; then
-		rm rootfs.ext*
-	fi
+create_uboot_ramdisk() {
+    if [ -f $fileName ]; then
+        rm rootfs.ext*
+    fi
 
-	# generate image
-	genext2fs -b 4096 -d rootfs $fileName
+    # generate image
+    genext2fs -b 4096 -d "$1" $fileName
 
-	# generate gzFileName
-	gzip -9 $fileName
+    # generate gzFileName
+    gzip -9 $fileName
 
-	# add ramdisk header
-	mkimage \
-		-n 'uboot ext2 ramdisk rootfs' \
-		-A arm \
-		-O linux \
-		-T ramdisk \
-		-C gzip \
-		-d $gzFileName \
-		$ubootFileName
+    # add ramdisk header
+    mkimage \
+        -n 'uboot ext4 ramdisk rootfs' \
+        -A arm \
+        -O linux \
+        -T ramdisk \
+        -C gzip \
+        -d $gzFileName \
+        $ubootFileName
 }
 
-create_ramdisk_by_qemu() {
-	qemu-img create $imgFileName 22m
-	mkfs.ext4 $imgFileName
-	if [ -d /tmp/tempfs ]; then
-		umount /tmp/tempfs > /dev/null 2>&1
-		rm -rf /tmp/tempfs
-	fi
-	mkdir /tmp/tempfs
-	mount -o loop $imgFileName /tmp/tempfs
-	sleep 1
-	rm -rf /tm/tempfs/*
-	if [ -f "rootfs/root/.bash_history" ]; then
-		rm rootfs/root/.bash_history
-	fi
-	cp -a rootfs/* /tmp/tempfs/
-	sync
-	sleep 1
-	umount /tmp/tempfs/
+create_qemu_ramdisk() {
+    qemu-img create $imgFileName "$1"
+    mkfs.ext4 $imgFileName
+    if [ -d /tmp/tempfs ]; then
+        umount /tmp/tempfs > /dev/null 2>&1
+        rm -rf /tmp/tempfs
+    fi
+    mkdir /tmp/tempfs
+    mount -o loop $imgFileName /tmp/tempfs
+    sleep 1
+    rm -rf /tm/tempfs/*
+    cp -a "$1"/* /tmp/tempfs/
+    sync
+    sleep 1
+    umount /tmp/tempfs/
 }
 
-if [ "$#" == 0 ]; then
-	echo "Start create ramdisk..."
-	create_ramdisk
+if [ "$1" == "uboot" ]; then
+    echo "Start create ramdisk..."
+    create_uboot_ramdisk "$1"
 elif [ "$1" == "qemu" ]; then
-	echo "Start create ramdisk by qemu..."
-	create_ramdisk_by_qemu
+    echo "Start create ramdisk by qemu..."
+    create_qemu_ramdisk "$1" "$2"
 else
-	echo "This script can create ramdisk, you can choose use qemu method."
-	echo "Usage: $0 [qemu]"
+    echo "This script can create ramdisk, you can choose use qemu method."
+    echo "Usage:"
+    echo "$0 uboot ROOTFS_DIR"
+    echo "$0 qemu ROOTFS_DIR ROOTFS_SIZE"
 fi
